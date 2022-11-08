5742e0615f6f61e3f89c6472e47306b19226f602
eltonfontesptlv <eltonfontesptlv@github.com>
2022-11-08 08:22:10 -0300

    POC init

---
+++ docker.sh
@@ -0,0 +1,10 @@
+docker run --name=migrations --rm \
+    -e FLYWAY_BASELINEONMIGRATE=$FLYWAY_BASELINEONMIGRATE \
+    -e FLYWAY_DRIVE=$FLYWAY_DRIVE \
+    -e FLYWAY_URL="${JDBC_URL}?disableSslHostnameVerification=${DB_USE_SSL:=false}&verifyServerCertificate=${DB_USE_SSL:=false}&useSSL=${DB_USE_SSL:=false}&requireSSL=${DB_USE_SSL:=false}&clientCertificateKeyStoreUrl=file:${DB_KEYSTORE_PATH:=''}&clientCertificateKeyStorePassword=${DB_KEYSTORE_PASSWORD:=''}&trustCertificateKeyStoreUrl=file:${DB_TRUSTSTORE_PATH:=''}&trustCertificateKeyStorePassword=${DB_TRUSTSTORE_PASSWORD:=''}" \
+    -e FLYWAY_USER=$FLYWAY_USER \
+    -e FLYWAY_PASS=$FLYWAY_PASS \
+    -v $PWD/sql/migrations:/flyway/sql \
+    -v $PWD/conf:/flyway/conf \
+    -v $PWD/certs:/flyway/certs \
+    flyway/flyway migrate
---
+++ run.sh
@@ -0,0 +1,5 @@
+docker run --name migrations --rm \
+    --env-file "./${APP_ENV:=dev}.env" \
+    -v $PWD/sql/migrations:/flyway/sql \
+    -v $PWD/conf:/flyway/conf \
+    flyway/flyway migrate
